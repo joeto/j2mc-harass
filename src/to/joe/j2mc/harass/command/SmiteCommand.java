e77f8fe6433e84b618fe23ff760f45354a522fa5
Ammar Askar <ammar_askar@hotmail.com>
2012-02-24 10:40:48 +0500

    Now causes damage and lights player on fire

--- src/to/joe/j2mc/harass/command/SmiteCommand.java
+++ src/to/joe/j2mc/harass/command/SmiteCommand.java
@@ -33,6 +33,8 @@ public class SmiteCommand extends MasterCommand {
                 //J2MC_Manager.getPermissions().setPermissions("j2mc.damage.take", true);
                 //this.j2.damage.addToTimer(target.getName());
                 target.getWorld().strikeLightningEffect(target.getLocation());
+                target.damage(20);
+                target.setFireTicks(200);
                 J2MC_Manager.getCore().adminAndLog(ChatColor.RED + sender.getName() + " has zapped " + target.getName());
                 target.sendMessage(ChatColor.RED + "You have been judged");
                 target.getWorld().setStorm(weather);
